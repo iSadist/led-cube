dc88fcd7035ee0c5c91808568acacc3027f4c672
Jan <jsvensson51@yahoo.com>
2024-01-29 17:59:14 +0100

    Stashed changes

--- led-cube.ino
+++ led-cube.ino
@@ -1,8 +1,10 @@
-int GROUND_PINS[] = {1, 2};
+int GROUND_PINS[] = {7, 4};
 int LAYER_PINS[] = {12, 11, 10, 9};
 int INHERIT_PIN = 13;
 
-int DELAY_MS = 50;
+int BUTTON_PINS[] = {A0, A1, A2};
+
+int DELAY_MS = 500;
 
 // Make a data structure to 4x4x4 cube leds
 
@@ -58,72 +60,46 @@ void set_pins(bool pins[4]) {
   }
 }
 
+
+
+void setBasePins(bool pins[2]) {
+  for (int i = 0; i < 2; i++) {
+    digitalWrite(GROUND_PINS[i], pins[i] ? HIGH : LOW);
+  }
+}
+
 void setup() {
   setup_pins();
+  Serial.begin(9600); // Serial monitor
+}
+
+void baseSweep() {
+  bool basePins[] = { false, false };
+  setBasePins(basePins);
+  delay(DELAY_MS);
+
+  bool basePins1[] = { true, false };
+  setBasePins(basePins1);
+  delay(DELAY_MS);
+
+  bool basePins2[] = { false, true };
+  setBasePins(basePins2);
+  delay(DELAY_MS);
+
+  bool basePins3[] = { true, true };
+  setBasePins(basePins3);
+  delay(DELAY_MS);
 }
 
 void loop() {
-  bool pins0[] = {false, false, false, false};
+
+  bool pins0[] = {false, false, true, false};
   set_pins(pins0);
-  delay(DELAY_MS);
+
+  baseSweep();
 
   bool pins1[] = {true, false, false, false};
   set_pins(pins1);
-  delay(DELAY_MS);
 
-  bool pins2[] = {false, true, false, false};
-  set_pins(pins2);
-  delay(DELAY_MS);
-
-  bool pins3[] = {true, true, false, false};
-  set_pins(pins3);
-  delay(DELAY_MS);
-
-  bool pins4[] = {false, false, true, false};
-  set_pins(pins4);
-  delay(DELAY_MS);
-
-  bool pins5[] = {true, false, true, false};
-  set_pins(pins5);
-  delay(DELAY_MS);
-
-  bool pins6[] = {false, true, true, false};
-  set_pins(pins6);
-  delay(DELAY_MS);
-
-  bool pins7[] = {true, true, true, false};
-  set_pins(pins7);
-  delay(DELAY_MS);
-
-  bool pins8[] = {false, false, false, true};
-  set_pins(pins8);
-  delay(DELAY_MS);
-
-  bool pins9[] = {true, false, false, true};
-  set_pins(pins9);
-  delay(DELAY_MS);
-
-  bool pins10[] = {false, true, false, true};
-  set_pins(pins10);
-  delay(DELAY_MS);
-
-  bool pins11[] = {true, true, false, true};
-  set_pins(pins11);
-  delay(DELAY_MS);
-
-  bool pins12[] = {false, false, true, true};
-  set_pins(pins12);
-  delay(DELAY_MS);
-
-  bool pins13[] = {true, false, true, true};
-  set_pins(pins13);
-  delay(DELAY_MS);
-
-  bool pins14[] = {false, true, true, true};
-  set_pins(pins14);
-  delay(DELAY_MS);
-
-  bool pins15[] = {true, true, true, true};
-  set_pins(pins15);
-  delay(DELAY_MS);
+  baseSweep();
 }
