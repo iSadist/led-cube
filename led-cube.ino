42c97da1e52f533f82c3051c548772587582d83b
Jan <jsvensson51@yahoo.com>
2024-02-02 10:03:01 +0100

    Remove unused functions

--- led-cube.ino
+++ led-cube.ino
@@ -3,20 +3,6 @@
 #include "pin-selection.h"
 #include "modes.h"
 
-// DOCS - if the user pressed any buttons, take action
-void listen_to_input() {}
-
-// DOCS - Return a binary sequence from a digital number. Example: 5 -> 0101, 3 -> 0011, 15 -> 1111
-int to_binary(int digital) {
-  return 0;
-}
-
-// DOCS - Translate the binary number to an array of the pins that should turn on.
-//        Send in a boolean if the pins is for the ground selection or layer pins.
-int binary_to_pins(int binary, bool is_ground_selection) {
-  return 0;
-}
-
 void setup_pins() {
   // Loop over LAYER_PINS and GROUND_PINS and set them to OUTPUT
   for (int i = 0; i < 4; i++) {
@@ -25,25 +11,6 @@ void setup_pins() {
   }
 
   pinMode(INHERIT_PIN, OUTPUT);
-}
-
-/**
- * Sweeps the base of the LED cube.
- * This function sequentially sets the base of the LED cube to different values,
- * causing a sweeping effect.
- * 
- * @param None
- * @return None
- */
-void baseSweep() {
-  setBase(0);
-  delay(DELAY_MS);
-  setBase(1);
-  delay(DELAY_MS);
-  setBase(2);
-  delay(DELAY_MS);
-  setBase(3);
-  delay(DELAY_MS);
 }
 
 // Main setup function
